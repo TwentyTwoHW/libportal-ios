3d313634b70e45d059312b0ddc990ffafdc76962
Alekos Filini <alekos.filini@gmail.com>
2024-03-29 19:41:29 +0100

    WIP

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-for f in "README.md" "LICENSE" "Sources" "libportal-ios.podspec" "Package.swift"
+for f in "README.md" "LICENSE" "Sources" "libportal-ios.podspec" "Package.swift" "portalFFI.xcframework" ".gitignore"
 do
 	rm -rf "$f"
 	cp -Rv "./portal-software/sdk/libportal-ios/$f" .
